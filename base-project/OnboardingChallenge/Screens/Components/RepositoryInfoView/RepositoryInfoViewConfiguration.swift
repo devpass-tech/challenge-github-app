ca988357ee705e62ed8377b3b6b99441541bf0d1
Fabio_Martinez <rok5150@gmail.com>
2021-10-15 15:55:37 -0300

    implementação do RepositoryInfoView

---
+++ base-project/OnboardingChallenge/Screens/Components/RepositoryInfoView/RepositoryInfoViewConfiguration.swift
@@ -0,0 +1,18 @@
+//
+//  RepositoryInfoViewConfiguration.swift
+//  OnboardingChallenge
+//
+//  Created by Fabio Martinez on 14/10/21.
+//
+
+import Foundation
+
+
+struct RepositoryInfoViewConfiguration {
+    
+        var name: String
+        var description: String
+        var stars: Int
+        var bifurcation: Int
+    
+    }
