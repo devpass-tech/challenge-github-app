677c89d0a91e1c790d7f01e26061788a353f3296
Maíra Preto <mairapreto@me.com>
2021-10-19 13:00:55 -0300

    implementação da tela de detalhes

---
+++ solutions/devsprint-4/OnboardingChallenge/Screens/Detail/DetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DetailsViewController.swift
+//  OnboardingChallenge
+//
+//  Created by Maíra Preto on 12/10/21.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
