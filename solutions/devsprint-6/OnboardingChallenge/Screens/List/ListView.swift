ebe5084f399137072dd9000c0f819ec698cc8a9a
Gustavo Soares <gustavosoares@Gustavo-Soares-MacBook-Pro-293A.local>
2021-11-09 22:16:40 -0300

    Creating Repository Cell
    Creating repository cell with new protocol called ViewCOde with methods for configure subviews, constraints and other configures

--- solutions/devsprint-6/OnboardingChallenge/Screens/List/ListView.swift
+++ solutions/devsprint-6/OnboardingChallenge/Screens/List/ListView.swift
@@ -9,7 +9,7 @@ import UIKit
 
 final class ListView: UIView {
 
-    private let listViewCellIdentifier = "ListViewCellIdentifier"
+    private let listViewCellIdentifier = String(describing: RepositoryCellView.self)
 
     private var listItems: [String] = []
 
@@ -19,45 +19,17 @@ final class ListView: UIView {
         tableView.translatesAutoresizingMaskIntoConstraints = false
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: self.listViewCellIdentifier)
         tableView.dataSource = self
+        tableView.delegate = self
         return tableView
     }()
 
     init() {
-
         super.init(frame: .zero)
-
-        self.customizeInterface()
+        setupViews()
     }
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
-    }
-}
-
-private extension ListView {
-
-    func customizeInterface() {
-
-        self.backgroundColor = .white
-
-        self.configureSubviews()
-        self.configureSubviewsConstraints()
-    }
-
-    func configureSubviews() {
-
-        self.addSubview(self.tableView)
-    }
-
-    func configureSubviewsConstraints() {
-
-        NSLayoutConstraint.activate([
-
-            self.tableView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
-            self.tableView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
-            self.tableView.topAnchor.constraint(equalTo: self.topAnchor),
-            self.tableView.bottomAnchor.constraint(equalTo: self.bottomAnchor)
-        ])
     }
 }
 
@@ -70,18 +42,53 @@ extension ListView {
     }
 }
 
-extension ListView: UITableViewDataSource {
+extension ListView: ViewCode {
+    
+    func configureSubViews() {
+        
+        self.addSubview(self.tableView)
+    }
+    
+    func configureSubviewsConstraints() {
+        
+        NSLayoutConstraint.activate([
+            
+            self.tableView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            self.tableView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            self.tableView.topAnchor.constraint(equalTo: self.topAnchor),
+            self.tableView.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+        ])
+    }
+    
+    func configureAdditionalBehaviors() {
+        
+        self.backgroundColor = .white
+    }
+}
+
+extension ListView: UITableViewDataSource, UITableViewDelegate {
 
     public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 
         return self.listItems.count
     }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        80
+    }
 
     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 
-        let cell = tableView.dequeueReusableCell(withIdentifier: self.listViewCellIdentifier)!
-        cell.textLabel?.text = self.listItems[indexPath.row]
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: self.listViewCellIdentifier) as? RepositoryCellView else {
+            return UITableViewCell()
+        }
+        
+        // TODO: removing mock repositoryOwnerName when have defined models
+        cell.updateView(with: RepositoryCellViewConfiguration(repositoryName: self.listItems[indexPath.row],
+                                                              repositoryOwnerName: "rdgborges"))
+        
         return cell
     }
 }
+
 
