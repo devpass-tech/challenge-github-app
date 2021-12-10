965424df65dd7854d94b31738bb0cf9e841aa7ff
Riccardo <riccardo.cipolleschi@gmail.com>
2021-12-10 09:39:51 +0100

    feat: add SwiftUIPreView to project

---
+++ solutions/devsprint-riccardo-1/OnboardingChallenge/Helpers/SwiftUIPreView.swift
@@ -0,0 +1,24 @@
+//
+//  SwiftUIPreView.swift
+//  OnboardingChallenge
+//
+//  Created by Riccardo Cipolleschi on 10/12/21.
+//
+
+import Foundation
+import SwiftUI
+
+struct SwiftUIPreView<V>: UIViewRepresentable where V: UIView {
+    
+    let viewFactory: (Context) -> V
+    
+    func makeUIView(context: Context) -> V {
+        return viewFactory(context)
+    }
+    
+    func updateUIView(_ uiView: V, context: Context) {
+        
+    }
+    
+    
+}
