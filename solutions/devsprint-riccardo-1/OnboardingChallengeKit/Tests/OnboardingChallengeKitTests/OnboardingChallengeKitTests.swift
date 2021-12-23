32a086e16f0a4c725a860f751ac5a8dfd3e4599f
Riccardo <riccardo.cipolleschi@gmail.com>
2021-12-23 10:14:31 +0100

    fix: remove code that generates a warning

--- solutions/devsprint-riccardo-1/OnboardingChallengeKit/Tests/OnboardingChallengeKitTests/OnboardingChallengeKitTests.swift
+++ solutions/devsprint-riccardo-1/OnboardingChallengeKit/Tests/OnboardingChallengeKitTests/OnboardingChallengeKitTests.swift
@@ -3,11 +3,4 @@
 import XCTest
 @testable import OnboardingChallengeKit
 
-final class OnboardingChallengeKitTests: XCTestCase {
-  func testExample() throws {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct
-    // results.
-//    XCTAssertEqual(OnboardingChallengeKit().text, "Hello, World!")
-  }
-}
+final class OnboardingChallengeKitTests: XCTestCase {}
