0d3d636a831dfa6f3a7615dc856a033db917b8c6
Pedro Francisco Tres <52545592+pedrotres@users.noreply.github.com>
2022-01-18 19:13:54 -0300

    call repositories

--- solutions/devsprint-rodrigo-borges-1/GitHubApp/Screens/List/ListViewController.swift
+++ solutions/devsprint-rodrigo-borges-1/GitHubApp/Screens/List/ListViewController.swift
@@ -8,41 +8,49 @@
 import UIKit
 
 final class ListViewController: UIViewController {
-
+    
     private lazy var listView: ListView = {
-
+        
         return ListView()
     }()
-
+    
     private let service = Service()
- 
+    
     init() {
         super.init(nibName: nil, bundle: nil)
-
+        
     }
-
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
+    
     override func loadView() {
         self.view = self.listView
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.fetchList()
+        self.fetchRepos()
     }
-
+    
+    private func fetchRepos(){
+        self.service.fetchRepositories{ user in
+            print(user)
+            
+        }
+    }
+    
     private func fetchList() {
-
+        
         self.service.fetchList { items in
-
+            
             let configuration = ListViewConfiguration(listItems: items)
-
+            
             self.listView.updateView(with: configuration)
         }
     }
+    
 }
 
 
