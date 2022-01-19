7ffae734a911693c26af83368fc4bdfc258f0752
Pedro Francisco Tres <52545592+pedrotres@users.noreply.github.com>
2022-01-19 20:50:23 -0300

    added method generic

--- solutions/devsprint-rodrigo-borges-1/GitHubApp/Service/Service.swift
+++ solutions/devsprint-rodrigo-borges-1/GitHubApp/Service/Service.swift
@@ -15,6 +15,10 @@ struct Service: ServiceProtocol {
     
     func fetchRepositories(_ name: String, _ completion: @escaping ([Repository]?) -> Void) {
         let url = URL(string:"https://api.github.com/users/\(name)/repos")!
+        self.request(url, completion: completion)
+    }
+    
+    func request<T: Decodable>(_ url: URL, completion: @escaping (T?) -> Void){
         
         let dataTask = URLSession.shared.dataTask(with: url){ data, response, error
             in
@@ -26,17 +30,19 @@ struct Service: ServiceProtocol {
             
             if let data = data {
                 let jsonDecodable = JSONDecoder()
-                let repo = try? jsonDecodable.decode([Repository].self, from: data)
-                completion(repo)
+                do{
+                    let repo = try jsonDecodable.decode(T.self, from: data)
+                    completion(repo)
+                }catch{
+                    print(error)
+                    completion(nil)
+                }
+       
             }
         }
         
         dataTask.resume()
         
-    }
-    
-    func fetchList(_ completion: ([String]) -> Void) {
-        completion(["item1", "item2", "item3", "item4"])
     }
     
 }
