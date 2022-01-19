893c3948b42ef080c35fddcde17dac9f0ea3020d
giovannamoeller <giovannagmoeller@gmail.com>
2022-01-19 11:29:39 -0300

    setting up owner view and controller

--- solutions/devsprint-attekita-1/GitHubApp/Screens/Owner/OwnerView.swift
+++ solutions/devsprint-attekita-1/GitHubApp/Screens/Owner/OwnerView.swift
@@ -9,12 +9,13 @@ import UIKit
 
 class OwnerView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
-
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    backgroundColor = .red
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
 }
