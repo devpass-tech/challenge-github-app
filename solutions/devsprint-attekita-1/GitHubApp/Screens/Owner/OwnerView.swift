2566230005d1168c244204e1ec24cff168910343
giovannamoeller <giovannagmoeller@gmail.com>
2022-01-19 11:26:43 -0300

    creating owner view and owner view controller

---
+++ solutions/devsprint-attekita-1/GitHubApp/Screens/Owner/OwnerView.swift
@@ -0,0 +1,20 @@
+//
+//  OwnerView.swift
+//  GitHubApp
+//
+//  Created by Giovanna Moeller on 19/01/22.
+//
+
+import UIKit
+
+class OwnerView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
