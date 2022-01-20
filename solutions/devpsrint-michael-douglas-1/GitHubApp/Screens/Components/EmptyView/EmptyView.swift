d7efc2356ecc737dd344da9cadf64bfeb56da93b
Gustavo Rocha <gustavo.rocha@usemobile.xyz>
2022-01-20 15:56:03 -0300

    🧩: created emptyView file

---
+++ solutions/devpsrint-michael-douglas-1/GitHubApp/Screens/Components/EmptyView/EmptyView.swift
@@ -0,0 +1,26 @@
+//
+//  EmptyView.swift
+//  GitHubApp
+//
+//  Created by Gustavo Rocha on 20/01/22.
+//
+
+import UIKit
+
+class EmptyView: UIView {
+
+    // MARK: Properties
+    
+    // MARK: Initialization
+    init() {
+        super.init(frame: .zero)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: Methods
+    
+    
+}
