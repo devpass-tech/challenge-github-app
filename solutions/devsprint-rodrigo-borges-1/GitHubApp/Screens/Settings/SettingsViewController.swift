e33a24da2ab039dd8c89e03534526569ecdd0ec9
Enrick Daltro <enrickdaltro@gmail.com>
2022-01-21 19:28:19 -0300

    feat: add Settings screen ViewController and UITableView configuration

--- solutions/devsprint-rodrigo-borges-1/GitHubApp/Screens/Settings/SettingsViewController.swift
+++ solutions/devsprint-rodrigo-borges-1/GitHubApp/Screens/Settings/SettingsViewController.swift
@@ -8,8 +8,27 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
-
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = .systemGray5
+        let proxy = UINavigationBar.appearance()
+        proxy.standardAppearance = appearance
+        proxy.scrollEdgeAppearance = appearance
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func loadView() {
         self.view = SettingsView()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Settings"
     }
 }
