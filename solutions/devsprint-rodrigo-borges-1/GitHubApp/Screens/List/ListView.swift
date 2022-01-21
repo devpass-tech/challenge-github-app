054b2311906d73e20660aa4ec5d684c59b72adf0
feeforcinetti <67073291+feeforcinetti@users.noreply.github.com>
2022-01-20 22:02:38 -0300

    IntegrarRepositoryCellView
    * Essa feature registra o componente RepositoryCellView na tableView da ListView
    * Implementado metodo que configura celulas com dados do Repository

--- solutions/devsprint-rodrigo-borges-1/GitHubApp/Screens/List/ListView.swift
+++ solutions/devsprint-rodrigo-borges-1/GitHubApp/Screens/List/ListView.swift
@@ -8,51 +8,50 @@
 import UIKit
 
 final class ListView: UIView {
-
-    private let listViewCellIdentifier = "ListViewCellIdentifier"
-
+    
     private var listItems: [Repository] = []
-
+    
     private lazy var tableView: UITableView = {
-
+        
         let tableView = UITableView(frame: .zero)
         tableView.translatesAutoresizingMaskIntoConstraints = false
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: self.listViewCellIdentifier)
+        tableView.register(RepositoryCellView.self, forCellReuseIdentifier: RepositoryCellView.classIdentifier())
         tableView.dataSource = self
+        tableView.delegate = self
         return tableView
     }()
-
+    
     init() {
-
+        
         super.init(frame: .zero)
-
+        
         self.customizeInterface()
     }
-
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 }
 
 private extension ListView {
-
+    
     func customizeInterface() {
-
+        
         self.backgroundColor = .white
-
+        
         self.configureSubviews()
         self.configureSubviewsConstraints()
     }
-
+    
     func configureSubviews() {
-
+        
         self.addSubview(self.tableView)
     }
-
+    
     func configureSubviewsConstraints() {
-
+        
         NSLayoutConstraint.activate([
-
+            
             self.tableView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
             self.tableView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
             self.tableView.topAnchor.constraint(equalTo: self.topAnchor),
@@ -62,26 +61,31 @@ private extension ListView {
 }
 
 extension ListView {
-
+    
     func updateView(with configuration: ListViewConfiguration) {
-
+        
         self.listItems = configuration.listRepositories
         self.tableView.reloadData()
     }
 }
 
-extension ListView: UITableViewDataSource {
-
+extension ListView: UITableViewDataSource, UITableViewDelegate {
+    
     public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-
+        
         return self.listItems.count
     }
-
+    
     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-
-        let cell = tableView.dequeueReusableCell(withIdentifier: self.listViewCellIdentifier)!
-        cell.textLabel?.text = self.listItems[indexPath.row].name
-        return cell
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: RepositoryCellView.classIdentifier(), for: indexPath) as? RepositoryCellView
+        cell?.updateRepositories(with: listItems[indexPath.row])
+        
+        return cell ?? UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 70
     }
 }
 
