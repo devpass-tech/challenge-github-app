b2a08a8a8b7ec6bd5832a0b1ab002f16ee407227
Silvano Malfatti <silvanomalfatti@gmail.com>
2022-01-22 20:34:06 -0300

    Inserting RepositoryInfoView into DetailView

---
+++ solutions/devsprint-attekita-1/GitHubApp/Screens/Components/RepositoryInfoView.swift
@@ -0,0 +1,93 @@
+
+import UIKit
+
+struct RepositoryInfoViewConfiguration {
+    let repositoryName: String
+    let repositoryDescription: String
+    let repositoryStars: Int
+    let repositoryForks: Int
+}
+
+final class RepositoryInfoView: UIView, ViewCodable {
+    
+    private lazy var verticalStack:UIStackView = {
+        let view  = UIStackView(frame: .zero)
+        view.alignment = .fill
+        view.spacing = 8
+        view.axis = .vertical
+        view.distribution = .fillProportionally
+        return view
+    }()
+    
+    private lazy var repositoryName:UILabel = {
+        let view  = UILabel(frame: .zero)
+        view.textColor = .black
+        view.textAlignment = .left
+        view.font = UIFont(name: "Verdana-Bold", size: 22)
+        return view
+    }()
+    
+    private lazy var repositoryDescription:UILabel = {
+        let view  = UILabel(frame: .zero)
+        view.textColor = .black
+        view.textAlignment = .left
+        view.numberOfLines = -1
+        view.font = UIFont(name: "Verdana", size: 14)
+        return view
+    }()
+    
+    private lazy var repositoryNumberStarsForks:UILabel = {
+        let view  = UILabel(frame: .zero)
+        view.textColor = .lightGray
+        view.textAlignment = .left
+        view.numberOfLines = 1
+        view.font = UIFont(name: "Verdana", size: 10)
+        return view
+    }()
+    
+    override init(frame:CGRect = .zero) {
+        super.init(frame: frame)
+        backgroundColor = .white
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func buildHierarchy() {
+        addSubview(verticalStack)
+        verticalStack.addArrangedSubview(repositoryName)
+        verticalStack.addArrangedSubview(repositoryDescription)
+        verticalStack.addArrangedSubview(repositoryNumberStarsForks)
+    }
+    
+    func setupConstraints() {
+        verticalStack.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        verticalStack.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+        verticalStack.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+        verticalStack.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        verticalStack.translatesAutoresizingMaskIntoConstraints = false
+        
+        repositoryName.topAnchor.constraint(equalTo: verticalStack.topAnchor, constant: 4).isActive = true
+        repositoryName.leadingAnchor.constraint(equalTo: verticalStack.leadingAnchor, constant: 8).isActive = true
+        repositoryName.trailingAnchor.constraint(equalTo: verticalStack.trailingAnchor, constant: -8).isActive = true
+        repositoryName.translatesAutoresizingMaskIntoConstraints = false
+        
+        repositoryDescription.leadingAnchor.constraint(equalTo: verticalStack.leadingAnchor, constant: 8).isActive = true
+        repositoryDescription.trailingAnchor.constraint(equalTo: verticalStack.trailingAnchor, constant: -8).isActive = true
+        repositoryDescription.translatesAutoresizingMaskIntoConstraints = false
+        
+        repositoryNumberStarsForks.leadingAnchor.constraint(equalTo: verticalStack.leadingAnchor, constant: 8).isActive = true
+        repositoryNumberStarsForks.trailingAnchor.constraint(equalTo: verticalStack.trailingAnchor, constant: -8).isActive = true
+        repositoryNumberStarsForks.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    func updateView(with configuration: RepositoryInfoViewConfiguration) {
+        repositoryName.text = configuration.repositoryName
+        repositoryDescription.text = configuration.repositoryDescription
+        repositoryNumberStarsForks.text = "\(configuration.repositoryStars)" + " estrelas " + "\(configuration.repositoryForks)" + " bifurcações"
+    }
+}
+
+
--- solutions/devsprint-attekita-1/GitHubApp/Screens/Detail/DetailView.swift
+++ solutions/devsprint-attekita-1/GitHubApp/Screens/Detail/DetailView.swift
@@ -7,15 +7,46 @@
 
 import UIKit
 
-final class DetailView: UIView {
+final class DetailView: UIView, ViewCodable {
+    
+    private lazy var verticalStack:UIStackView = {
+        let view  = UIStackView(frame: .zero)
+        view.alignment = .fill
+        view.spacing = 8
+        view.axis = .vertical
+        view.distribution = .fillProportionally
+        return view
+    }()
+    
+    private lazy var repositoryInfoView: RepositoryInfoView = RepositoryInfoView()
     
     init() {
         super.init(frame: .zero)
         backgroundColor = .white
+        
+        setupView()
+        
+        repositoryInfoView.updateView(with: RepositoryInfoViewConfiguration(repositoryName: "Dev Sprint Attekita", repositoryDescription: "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum" , repositoryStars: 5, repositoryForks: 20))
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    func buildHierarchy() {
+        addSubview(verticalStack)
+        verticalStack.addArrangedSubview(repositoryInfoView)
+    }
+    
+    func setupConstraints() {
+        verticalStack.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        verticalStack.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+        verticalStack.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+        verticalStack.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        verticalStack.translatesAutoresizingMaskIntoConstraints = false
+        
+        repositoryInfoView.leadingAnchor.constraint(equalTo:verticalStack.leadingAnchor).isActive = true
+        repositoryInfoView.trailingAnchor.constraint(equalTo:verticalStack.trailingAnchor).isActive = true
+        repositoryInfoView.translatesAutoresizingMaskIntoConstraints = false
+    }
 }
