6a636153bbb87c639649def28bf8610b53afe157
Fabio_Martinez <rok5150@gmail.com>
2022-01-24 14:49:56 -0300

    add loadingView

---
+++ solutions/devpsrint-michael-douglas-1/GitHubApp/Screens/Components/LoadingViewConfiguration/LoadingViewConfiguration.swift
@@ -0,0 +1,16 @@
+//
+//  LoadingViewConfiguration.swift
+//  GitHubApp
+//
+//  Created by Fabio Martinez on 21/01/22.
+//
+
+import Foundation
+
+struct LoadingViewConfiguration {
+    let textLabel : String
+}
+
+
+
+
--- solutions/devpsrint-michael-douglas-1/GitHubApp/Screens/List/ListView.swift
+++ solutions/devpsrint-michael-douglas-1/GitHubApp/Screens/List/ListView.swift
@@ -8,77 +8,86 @@
 import UIKit
 
 final class ListView: UIView {
-
+    
     private let listViewCellIdentifier = "ListViewCellIdentifier"
-
+    
     private var repositories: [Repository] = []
-
+    
     private lazy var tableView: UITableView = {
-
+        
         let tableView = UITableView(frame: .zero)
         tableView.translatesAutoresizingMaskIntoConstraints = false
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: self.listViewCellIdentifier)
         tableView.dataSource = self
         return tableView
     }()
-
+    
+    private lazy var loadingView: LoadingView = {
+        let loadingView = LoadingView()
+        loadingView.updateView(with: LoadingViewConfiguration(textLabel: "Carregando"))
+        loadingView.translatesAutoresizingMaskIntoConstraints = false
+        return loadingView
+    }()
+    
     init() {
-
         super.init(frame: .zero)
-
         self.customizeInterface()
     }
-
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 }
 
 private extension ListView {
-
+    
     func customizeInterface() {
-
+        
         self.backgroundColor = .white
-
         self.configureSubviews()
         self.configureSubviewsConstraints()
     }
-
+    
     func configureSubviews() {
-
+        
         self.addSubview(self.tableView)
+        self.addSubview(self.loadingView)
     }
-
+    
     func configureSubviewsConstraints() {
-
+        
         NSLayoutConstraint.activate([
-
+            
             self.tableView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
             self.tableView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
             self.tableView.topAnchor.constraint(equalTo: self.topAnchor),
-            self.tableView.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+            self.tableView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+            self.loadingView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            self.loadingView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            self.loadingView.topAnchor.constraint(equalTo: self.topAnchor),
+            self.loadingView.bottomAnchor.constraint(equalTo: self.bottomAnchor)
         ])
     }
 }
 
 extension ListView {
-
+    
     func updateView(with configuration: ListViewConfiguration) {
-
+        
         self.repositories = configuration.repositories
         self.tableView.reloadData()
     }
 }
 
 extension ListView: UITableViewDataSource {
-
+    
     public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-
+        
         return self.repositories.count
     }
-
+    
     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: self.listViewCellIdentifier)!
         cell.textLabel?.text = self.repositories[indexPath.row].name
         return cell
