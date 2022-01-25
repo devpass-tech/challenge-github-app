05703ad95df382558b27c0c455a01bc2fa6ff547
Michelli <contato@michellicristina.com.br>
2022-01-25 20:45:37 -0300

    fix: name

--- solutions/devsprint-attekita-1/GitHubApp/Service/Service.swift
+++ solutions/devsprint-attekita-1/GitHubApp/Service/Service.swift
@@ -12,7 +12,7 @@ struct Service {
     
     func fetchList(userName: String, completion: @escaping ([GithubApp]) -> Void) {
         
-        GitHubAPI.loadRepos(name: "") { (repos) in
+        GitHubAPI.loadRepos(name: userName) { (repos) in
                 
                 completion(repos)
             
