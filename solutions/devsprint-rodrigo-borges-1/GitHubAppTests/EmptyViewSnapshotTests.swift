59a5f3b02ba848708aba8e3c63f572778e839e2c
Pedro Francisco Tres <52545592+pedrotres@users.noreply.github.com>
2022-01-27 12:26:27 -0300

    Added Empty View SnapshotTesting

---
+++ solutions/devsprint-rodrigo-borges-1/GitHubAppTests/EmptyViewSnapshotTests.swift
@@ -0,0 +1,28 @@
+//
+//  EmptyViewSnapshotTests.swift
+//  GitHubAppTests
+//
+//  Created by Pedro Francisco Tres on 27/01/22.
+//
+
+import XCTest
+import SnapshotTesting
+@testable import GitHubApp
+class EmptyViewSnapshotTests: XCTestCase {
+    
+    let size = CGSize(width: 350, height: 600)
+
+    func testEmptyView() throws {
+        let configuration = EmptyViewConfiguration(title: "No repositories found", subtitle: "Search for users to see their public repositories here!")
+        
+        let emptyView = EmptyView()
+        emptyView.backgroundColor = .white
+        emptyView.updateView(with: configuration)
+        
+        assertSnapshot(matching: emptyView, as: .image(size: size), record: true)
+    
+    }
+
+
+
+}
