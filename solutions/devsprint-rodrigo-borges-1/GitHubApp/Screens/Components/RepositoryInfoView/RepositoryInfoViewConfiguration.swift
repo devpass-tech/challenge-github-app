f38ee0b4897026736adb975ace29e3b4f30a2e85
Pedro Francisco Tres <52545592+pedrotres@users.noreply.github.com>
2022-01-27 21:28:01 -0300

    Added RepositoryInfoView component

---
+++ solutions/devsprint-rodrigo-borges-1/GitHubApp/Screens/Components/RepositoryInfoView/RepositoryInfoViewConfiguration.swift
@@ -0,0 +1,15 @@
+//
+//  RepositoryInfoViewConfiguration.swift
+//  GitHubApp
+//
+//  Created by Pedro Francisco Tres on 27/01/22.
+//
+
+import Foundation
+
+struct RepositoryInfoViewConfiguration {
+    var title: String
+    var description: String
+    var stars: Int
+    var bifurcations: Int
+}
