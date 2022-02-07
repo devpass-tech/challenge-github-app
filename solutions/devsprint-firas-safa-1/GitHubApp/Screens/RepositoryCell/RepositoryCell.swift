e05560f49212c3a05a0f60855b256c7a72af31ea
Pedro Neto <pedro.santosnt@hotmail.com>
2022-02-07 18:06:55 -0300

    create the RepositoryCell

---
+++ solutions/devsprint-firas-safa-1/GitHubApp/Screens/RepositoryCell/RepositoryCell.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+class RepositoryCell: UITableViewCell {
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupCell() {
+        
+    }
+
+    private func setupCellLayout() {
+        
+    }
+}
