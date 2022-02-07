3ed342f3a521aa283baa9cf3ec67c53dc3e97ed7
Pedro Neto <pedro.santosnt@hotmail.com>
2022-02-07 18:08:10 -0300

    create RepositoryCell components

--- solutions/devsprint-firas-safa-1/GitHubApp/Screens/RepositoryCell/RepositoryCell.swift
+++ solutions/devsprint-firas-safa-1/GitHubApp/Screens/RepositoryCell/RepositoryCell.swift
@@ -10,11 +10,38 @@ class RepositoryCell: UITableViewCell {
         fatalError("init(coder:) has not been implemented")
     }
     
+    private lazy var mainTitle: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .boldSystemFont(ofSize: 15)
+        return label
+    }()
+    
+    private lazy var subTitle: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .systemFont(ofSize: 13)
+        label.textColor = UIColor(red: 0.235, green: 0.235, blue: 0.263, alpha: 0.6)
+        return label
+    }()
+    
     private func setupCell() {
         
     }
 
     private func setupCellLayout() {
+        addSubview(mainTitle)
+        addSubview(subTitle)
         
+        NSLayoutConstraint.activate([
+            mainTitle.topAnchor.constraint(equalTo: topAnchor, constant: 17),
+            mainTitle.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            mainTitle.trailingAnchor.constraint(equalTo: trailingAnchor),
+            
+            subTitle.topAnchor.constraint(equalTo: mainTitle.bottomAnchor),
+            subTitle.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            subTitle.trailingAnchor.constraint(equalTo: trailingAnchor),
+            subTitle.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -17),
+        ])
     }
 }
