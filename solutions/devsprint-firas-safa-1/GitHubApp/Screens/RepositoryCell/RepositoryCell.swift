9e529115efd4332874661e5721b6b7a4f1184a30
PsN-1 <67563603+PsN-1@users.noreply.github.com>
2022-02-08 15:14:53 -0300

    Update solutions/devsprint-firas-safa-1/GitHubApp/Screens/RepositoryCell/RepositoryCell.swift
    Co-authored-by: TheInkedEngineer <firas@hey.com>

--- solutions/devsprint-firas-safa-1/GitHubApp/Screens/RepositoryCell/RepositoryCell.swift
+++ solutions/devsprint-firas-safa-1/GitHubApp/Screens/RepositoryCell/RepositoryCell.swift
@@ -7,7 +7,6 @@ struct RepositoryCellViewConfiguration {
 }
 
 class RepositoryCell: UITableViewCell {
-    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         setupCellLayout()
