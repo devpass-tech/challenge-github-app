9e6b41f696bfda94d74f4858c73f870a25024286
Pedro Neto <pedro.santosnt@hotmail.com>
2022-02-09 21:47:44 -0300

    Implement SettingsView

--- solutions/devsprint-firas-safa-1/GitHubApp/Screens/Settings/SettingsView.swift
+++ solutions/devsprint-firas-safa-1/GitHubApp/Screens/Settings/SettingsView.swift
@@ -7,6 +7,62 @@
 
 import UIKit
 
+private let cellIdentifier = "SettingsCell"
+
 class SettingsView: UIView {
+    private let viewModel: SettingsViewModel
+    private let tableView = UITableView()
     
+    init(viewModel: SettingsViewModel = SettingsViewModel()) {
+        self.viewModel = viewModel
+        super.init(frame: .zero)
+        
+        setupSubviews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension SettingsView {
+    func setupSubviews() {
+        addSubview(tableView)
+        
+        setupTableView()
+        setupSubviewsConstraints()
+    }
+    
+    func setupTableView() {
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
+        tableView.dataSource = self
+    }
+    
+    func setupSubviewsConstraints() {
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            self.tableView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            self.tableView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            self.tableView.topAnchor.constraint(equalTo: topAnchor),
+            self.tableView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
+}
+
+extension SettingsView: UITableViewDataSource  {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        viewModel.rowCount
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        
+        cell.textLabel?.text = viewModel.getCellFor(indexPath.row)
+        cell.selectionStyle = .none
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        viewModel.getHeaderFor(section)
+    }
 }
