e6c5789d480a0356c4924ce253f032539ca853de
Renato Sarro <cora@MacBook-Pro-de-Cora-7.local>
2022-05-23 18:56:59 -0300

    Implementa service.

--- solutions/devsprint-renato-sarro-3/GitHubApp/Service/Service.swift
+++ solutions/devsprint-renato-sarro-3/GitHubApp/Service/Service.swift
@@ -11,6 +11,6 @@ struct Service {
 
     func fetchList(_ completion: ([String]) -> Void) {
 
-        completion(["Repository 1", "Repository 2", "Repository 3"])
+        completion(["Repository 1", "Repository 2", "Repository 3", "Repository 4"])
     }
 }
