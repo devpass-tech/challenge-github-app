9ea14a12ccca6a259941cdb20c4de8180cc3064b
Thyago Raphael <thyagoraphael1987@gmail.com>
2022-05-25 11:38:52 -0300

    - Adicionado UITableView monstrando os repositórios a partir de uma fake list.

--- solutions/devsprint-renato-sarro-3/GitHubApp/Screens/List/ListView.swift
+++ solutions/devsprint-renato-sarro-3/GitHubApp/Screens/List/ListView.swift
@@ -8,85 +8,78 @@
 import UIKit
 
 struct ListViewConfiguration {
-
-    let listItems: [String]
+    let listItems: [RepositoryCellViewConfiguration]
 }
 
 final class ListView: UIView {
-
-    private let listViewCellIdentifier = "ListViewCellIdentifier"
-
-    private var listItems: [String] = []
-
+    
+    // MARK: - Properties
+    private var listItems: [RepositoryCellViewConfiguration] = []
+    
+    // MARK: - UI Components
     private lazy var tableView: UITableView = {
-
-        let tableView = UITableView(frame: .zero)
+        let tableView = UITableView(frame: .zero, style: .plain)
         tableView.translatesAutoresizingMaskIntoConstraints = false
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: self.listViewCellIdentifier)
+        tableView.register(ListViewCell.self, forCellReuseIdentifier: ListViewCell.identifier)
+        tableView.delegate = self
         tableView.dataSource = self
         return tableView
     }()
-
+    
+    // MARK: - Initializations
     init() {
-
         super.init(frame: .zero)
-
-        self.setupViews()
+        setupViewCode()
     }
-
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 }
 
-private extension ListView {
-
-    func setupViews() {
-
-        self.backgroundColor = .white
-
-        self.configureSubviews()
-        self.configureSubviewsConstraints()
+// MARK: - Extensions
+extension ListView {
+    func updateView(with configuration: ListViewConfiguration) {
+        self.listItems = configuration.listItems
+        self.tableView.reloadData()
     }
+}
 
-    func configureSubviews() {
-
+extension ListView: ViewCode {
+    func buildHierarchy() {
         self.addSubview(self.tableView)
     }
-
-    func configureSubviewsConstraints() {
-
+    
+    func setupConstraints() {
         NSLayoutConstraint.activate([
-
             self.tableView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
             self.tableView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
             self.tableView.topAnchor.constraint(equalTo: self.topAnchor),
             self.tableView.bottomAnchor.constraint(equalTo: self.bottomAnchor)
         ])
     }
+    
+    func setupExtraConfiguration() {
+        self.backgroundColor = .white
+    }
 }
 
-extension ListView {
-
-    func updateView(with repositories: [String]) {
-
-        self.listItems = repositories
-        self.tableView.reloadData()
-    }
+extension ListView: UITableViewDelegate {
+    
 }
 
 extension ListView: UITableViewDataSource {
-
     public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-
         return self.listItems.count
     }
-
-    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-
-        let cell = tableView.dequeueReusableCell(withIdentifier: self.listViewCellIdentifier)!
-        cell.textLabel?.text = self.listItems[indexPath.row]
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: ListViewCell.identifier, for: indexPath) as? ListViewCell else {
+            return UITableViewCell()
+        }
+        
+        cell.updateView(with: self.listItems[indexPath.row])
+    
         return cell
     }
 }
-
