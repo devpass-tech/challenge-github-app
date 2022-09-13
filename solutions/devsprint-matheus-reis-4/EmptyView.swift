af5a26cbba6091e7337008b5bdaa7b6197a47532
Jonas <jonassilvajns@icloud.com>
2022-09-12 22:48:12 -0300

    Feature/EmptyView

---
+++ solutions/devsprint-matheus-reis-4/EmptyView.swift
@@ -0,0 +1,9 @@
+//
+//  EmptyView.swift
+//  GitHubApp
+//
+//  Created by Jonas Silva on 12/09/22.
+//
+
+
+
---
+++ solutions/devsprint-matheus-reis-4/GitHubApp/Screens/Components/EmptyView/EmptyViewController.swift
@@ -0,0 +1,30 @@
+//
+//  EmptyViewController.swift
+//  GitHubApp
+//
+//  Created by Jonas Silva on 12/09/22.
+//
+
+import UIKit
+
+class EmptyView: UIViewController {
+
+    // MARK: - IBOutlet
+    
+    @IBOutlet weak var lbNoRepository: UILabel!
+    @IBOutlet weak var lbSearchForUsers: UILabel!
+    
+    
+
+    // MARK: - Properties
+    
+    // MARK: - Super Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    // MARK: - Methods
+    
+    // MARK: - IBActions
+    
+    
+}
