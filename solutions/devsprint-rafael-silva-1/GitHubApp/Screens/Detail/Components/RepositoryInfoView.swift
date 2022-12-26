530e1ba9edd91d0cda2373eb2b3094738db99db9
Frédéric Helfer <fre.helfer@gmail.com>
2022-12-26 20:43:12 -0300

    added Labels

--- solutions/devsprint-rafael-silva-1/GitHubApp/Screens/Detail/Components/RepositoryInfoView.swift
+++ solutions/devsprint-rafael-silva-1/GitHubApp/Screens/Detail/Components/RepositoryInfoView.swift
@@ -7,14 +7,88 @@
 
 import UIKit
 
+struct RepositoryInfoViewConfiguration {
+    let repoTitle: String
+    let repoDescription: String
+    let stars: Int
+    let forks: Int
+}
+
 class RepositoryInfoView: UIView {
+    
+    lazy var repoTitle: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 22, weight: .bold)
+        return label
+    }()
+    
+    lazy var repoDescription: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 15, weight: .regular)
+        return label
+    }()
+    
+    lazy var starsAndForks: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 12, weight: .regular)
+        label.textColor = .lightGray
+        return label
+    }()
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
     }
-    */
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
 
+private extension RepositoryInfoView {
+    
+    func setupViews() {
+        
+        configureSubviews()
+        configureSubviewsConstraints()
+    }
+    
+    func configureSubviews() {
+        addSubview(repoTitle)
+        addSubview(repoDescription)
+        addSubview(starsAndForks)
+    }
+    
+    func configureSubviewsConstraints() {
+        NSLayoutConstraint.activate([
+        
+            repoTitle.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 15),
+            repoTitle.topAnchor.constraint(equalTo: topAnchor, constant: 21),
+            repoTitle.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -18),
+            
+            repoDescription.leadingAnchor.constraint(equalTo: repoTitle.leadingAnchor),
+            repoDescription.trailingAnchor.constraint(equalTo: repoTitle.trailingAnchor),
+            repoDescription.topAnchor.constraint(equalTo: repoTitle.bottomAnchor, constant: 9),
+            
+            starsAndForks.leadingAnchor.constraint(equalTo: repoTitle.leadingAnchor),
+            starsAndForks.trailingAnchor.constraint(equalTo: repoTitle.trailingAnchor),
+            starsAndForks.topAnchor.constraint(equalTo: repoDescription.bottomAnchor, constant: 13),
+            starsAndForks.bottomAnchor.constraint(equalTo: bottomAnchor, constant: 18.5)
+            
+        ])
+    }
+}
+
+extension RepositoryInfoView {
+    
+    public func updateView(with configuration: RepositoryInfoViewConfiguration) {
+        self.repoTitle.text = configuration.repoTitle
+        self.repoDescription.text = configuration.repoDescription
+        self.starsAndForks.text = "\(configuration.stars) estrelas  \(configuration.forks) bifurcações"
+    }
+    
 }
