c9564ddd0e921f670bd675a9398fa10d3f636bdb
Frédéric Helfer <fre.helfer@gmail.com>
2022-12-26 20:02:42 -0300

    created RepositoryInfoView

---
+++ solutions/devsprint-rafael-silva-1/GitHubApp/Screens/Detail/Components/RepositoryInfoView.swift
@@ -0,0 +1,20 @@
+//
+//  RepositoryInfoView.swift
+//  GitHubApp
+//
+//  Created by Frédéric Helfer on 26/12/22.
+//
+
+import UIKit
+
+class RepositoryInfoView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
