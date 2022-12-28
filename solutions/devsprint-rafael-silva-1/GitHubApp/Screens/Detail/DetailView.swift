07a75cef543a6a715897914b41a2819a05fae836
Frédéric Helfer <fre.helfer@gmail.com>
2022-12-28 12:39:36 -0300

    Implemented repositoryInfoView on DetailView

--- solutions/devsprint-rafael-silva-1/GitHubApp/Screens/Detail/DetailView.swift
+++ solutions/devsprint-rafael-silva-1/GitHubApp/Screens/Detail/DetailView.swift
@@ -9,4 +9,44 @@ import UIKit
 
 class DetailView: UIView {
     
+    lazy var repositoryInfoView: RepositoryInfoView = {
+        let view = RepositoryInfoView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.updateView(with: RepositoryInfoViewConfiguration(repoTitle: "Título", repoDescription: "Descrição", stars: 15, forks: 4))
+        return view
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+extension DetailView {
+    
+    func setupViews() {
+        backgroundColor = .systemBackground
+        
+        configureSubviews()
+        configureSubviewsConstraints()
+    }
+    
+    func configureSubviews() {
+        addSubview(repositoryInfoView)
+    }
+    
+    func configureSubviewsConstraints() {
+        NSLayoutConstraint.activate([
+        
+            repositoryInfoView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            repositoryInfoView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            repositoryInfoView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            
+        ])
+    }
 }
