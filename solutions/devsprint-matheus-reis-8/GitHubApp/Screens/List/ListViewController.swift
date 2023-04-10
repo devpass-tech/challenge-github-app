d28661d92ee7c195e2af151a8686a2d0d81ae9b9
Lucas Vieira <74318102+LucasVieiraa@users.noreply.github.com>
2023-04-10 18:36:14 -0300

    :sparkles: feat: add navigation-bar (#198)
    Co-authored-by: lucas vieira <lucasgomes114@gmai.com>

--- solutions/devsprint-matheus-reis-8/GitHubApp/Screens/List/ListViewController.swift
+++ solutions/devsprint-matheus-reis-8/GitHubApp/Screens/List/ListViewController.swift
@@ -9,16 +9,22 @@ import UIKit
 final class ListViewController: UIViewController {
 
     private let listView: ListView = {
-
         let listView = ListView()
         return listView
+    }()
+    
+    private lazy var searchController: UISearchController = {
+        let searchController = UISearchController()
+        searchController.searchBar.delegate = self
+        searchController.searchBar.placeholder = "Type a GitHub user name"
+        
+        return searchController
     }()
 
     private let service = Service()
 
     init() {
         super.init(nibName: nil, bundle: nil)
-
     }
 
     required init?(coder: NSCoder) {
@@ -26,24 +32,37 @@ final class ListViewController: UIViewController {
     }
 
     override func viewDidLoad() {
-
-        self.navigationController?.navigationBar.prefersLargeTitles = true
-        self.navigationItem.title = "GitHub App 🐙"
+        setupNavigationBar()
     }
 
     override func viewDidAppear(_ animated: Bool) {
-
         service.fetchList { repositories in
-
             DispatchQueue.main.async {
-
                 self.listView.updateView(with: repositories)
             }
         }
-
     }
 
     override func loadView() {
         self.view = listView
+    }
+    
+    private func setupNavigationBar() {
+        let navBarAppearance = UINavigationBarAppearance()
+        navBarAppearance.configureWithOpaqueBackground()
+        navBarAppearance.backgroundColor = UIColor(red: 245, green: 245, blue: 245)
+        
+        navigationController?.navigationBar.scrollEdgeAppearance = navBarAppearance
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.title = "Repositories"
+        navigationItem.searchController = searchController
+    }
+}
+
+extension ListViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        let text = searchBar.text
+        searchController.isActive = false
+        searchBar.text = text
     }
 }
